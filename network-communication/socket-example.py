fc526e5967d3db176997b85d3694649881ab7de8
zhaoxiaojia <799833928@qq.com>
2020-03-30 19:35:48 +0800

    mar-30-office-daily

--- network-communication/socket-example.py
+++ network-communication/socket-example.py
@@ -9,12 +9,15 @@
 
 
 import socket
-
+'''
+socket.SOCK_DGRAM  UDP连接
+socket.SOCK_STREAM TCP连接
+'''
 if __name__ == '__main__':
     # 创建一个UDP
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     # 可以使用套接字收发数据
-    udp_socket.sendto('Fuck the world',('127.0.0.1',8080))
+    udp_socket.sendto(b'Fuck the world', ("10.18.19.100",8080))
     # 关闭套接字
     udp_socket.close()
