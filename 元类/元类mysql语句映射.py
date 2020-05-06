dd58e2acfdcb48912b7140a3f953652963215cdb
zhaoxiaojia <799833928@qq.com>
2020-05-06 19:57:34 +0800

    Office-May-6

---
+++ 元类/元类mysql语句映射.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/5/6 18:17
+# @Author  : Coco
+# @Site    : SH #5-389
+# @File    : 元类mysql语句映射.py
+# @Email   : chao.li@amlogic.com
+# @Software: PyCharm
+
+class ModelMetaclass(type):
+    def __new__(cls, name, bases, attrs):
+        mappings = dict()
+        for key, value in attrs.items():
+            # 判断是否是元祖类型 是则保存
+            if isinstance(value, tuple):
+                print('Found mapping ：%s ==> %s', (key, value))
+                mappings[key] = value
+        # 删除字典里的属性
+        for key in list(attrs.keys()):
+            attrs.pop(key)
+        # 重新添加属性
+        attrs['__mappings__'] = mappings
+        attrs['__table__'] = name
+        return type.__new__(cls, name, bases, attrs)
+
+
+class User(metaclass=ModelMetaclass):
+    uid = ('uid', 'int unsigned')
+    name = ('username', 'varchar(30)')
+    email = ('email', 'varchar(30)')
+    password = ('password', 'varchar(30)')
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def save(self):
+        fields = list()
+        args = list()
+        for key, value in self.__mapping__.items():
+            fields.append(value[0])
+            args.append(getattr(self, key, None))
+
+        sql = 'insert into %s (%s) value (%s)' % (self.__table__, ','.join(fields), ','.join([str(i) for i in args]))
+        print('SQL：' + sql)
+
+
+u = User(uid=12345, name='coco', email='xxx@xx.com', password='123')
+u.save()
